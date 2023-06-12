1c18288924ffe8e81c106d7888947cd345dfb3e7
Md Arifur Rahman <sojibkhanarif999@gmail.com>
2023-06-12 23:14:51 +0600

    add animation to home

--- src/component/pages/Home/home/Home.jsx
+++ src/component/pages/Home/home/Home.jsx
@@ -3,13 +3,28 @@ import Banner from './Banner';
 import Slider from './Slider';
 import PopularClass from './PopularClass';
 import Instructor from './Instructor';
+import { Fade } from "react-awesome-reveal";
 
+// function App() {
+//   return (
+
+//   );
+// }
 
 const Home = () => {
     return (
         <div>
 
             <Banner></Banner>
+
+            <Fade>
+                <p className='text-white text-4xl'>I am an animated text</p>
+            </Fade>
+            <Fade cascade>
+                <p>I enter first...</p>
+                <p>...then comes my turn...</p>
+                <p>...and finally you see me!</p>
+            </Fade>
             <Slider></Slider>
             <PopularClass></PopularClass>
             <Instructor></Instructor>
